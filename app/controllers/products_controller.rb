8ae0ace7d5bd3052a5898b1526d75ae071c2d7f3
Unknown <stanshalomchin@yahoo.com>
2017-03-10 14:09:41 +0800

    Add kaminari

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(15)
   end
 
   # GET /products/1
