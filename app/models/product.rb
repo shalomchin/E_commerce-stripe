f5d92ef536f93f977d9998a365454941444398b0
Unknown <stanshalomchin@yahoo.com>
2017-03-12 12:33:15 +0800

    Add labels and styled forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
-  
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }, uniqueness: true
+  validates :price, :format => { with: /\A(\$)?(\d+)(\.|,)?\d{0,2}?\z/  ,  message: "no alphabets allowed" }
+  validates :price, presence: true
+  validates :description, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+  validates :stock, presence: true, numericality: { only_integer: true }
 end
