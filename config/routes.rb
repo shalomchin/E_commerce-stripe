dffc195f0b72c2444db5f3abcedc238a4013aabb
Unknown <stanshalomchin@yahoo.com>
2017-03-12 14:20:58 +0800

    Add more forms to products index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :products do
+  resources :products 
     resources :orders
-  end
+  
   get 'products/index'
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
